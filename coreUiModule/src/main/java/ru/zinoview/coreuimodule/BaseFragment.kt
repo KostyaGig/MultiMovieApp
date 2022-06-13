678f9aee89eb21c817106dfc663be538a6d45f4e
kostazinovev <k.gig@list.ru>
2022-06-13 14:05:32 +0300

    core ui module base fragment

---
+++ coreUiModule/src/main/java/ru/zinoview/coreuimodule/BaseFragment.kt
@@ -0,0 +1,31 @@
+package ru.zinoview.coreuimodule
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<B : ViewBinding>(
+    @LayoutRes id: Int
+) : Fragment(id) {
+
+    private var _binding: B? = null
+
+    protected val binding by lazy {
+        checkNotNull(_binding)
+    }
+    abstract fun initBinding(inflater: LayoutInflater,container: ViewGroup) : B
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = initBinding(inflater,container!!)
+        return _binding?.root
+    }
+}
