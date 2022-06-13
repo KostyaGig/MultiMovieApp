0e101ccff6077914b7411b2bf6f105c66556d6f2
kostazinovev <k.gig@list.ru>
2022-06-13 13:54:33 +0300

    create component provider as an Application

--- app/src/main/java/ru/zinoview/multimovieapp/MovieApp.kt
+++ app/src/main/java/ru/zinoview/multimovieapp/MovieApp.kt
@@ -1,14 +1,15 @@
 package ru.zinoview.multimovieapp
 
 import android.app.Application
+import ru.zinoview.core.MovieApplication
 import ru.zinoview.core.di.CoreComponent
 import ru.zinoview.core.di.DaggerCoreComponent
 import ru.zinoview.movies.presentation.di.DaggerMoviesComponent
 import ru.zinoview.movies.presentation.di.MoviesComponent
 
-class MovieApp : Application() {
+class MovieApp : Application() , MovieApplication<MoviesComponent> {
 
-    lateinit var coreComponent: CoreComponent
+    private lateinit var coreComponent: CoreComponent
     lateinit var moviesComponent: MoviesComponent
 
     override fun onCreate() {
@@ -17,5 +18,7 @@ class MovieApp : Application() {
         coreComponent = DaggerCoreComponent.builder().bindContext(this).build()
         moviesComponent = DaggerMoviesComponent.builder().coreComponent(coreComponent).build()
     }
+
+    override fun component() = moviesComponent
 
 }
