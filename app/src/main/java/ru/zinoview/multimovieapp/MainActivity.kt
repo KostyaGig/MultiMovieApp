c6b817359a0c147b0903caaa9ec103f979f45dfe
kostazinovev <k.gig@list.ru>
2022-06-13 19:39:24 +0300

    create feature movie

--- app/src/main/java/ru/zinoview/multimovieapp/MainActivity.kt
+++ app/src/main/java/ru/zinoview/multimovieapp/MainActivity.kt
@@ -15,24 +15,25 @@ class MainActivity : AppCompatActivity(), Navigation, Finish {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
     }
 
     // todo make back nav
-    override fun onBackPressed() {
-        val fragment = navHostFragment!!.childFragmentManager.fragments.first()
-        if (fragment is  BaseFragment<*>) {
-            fragment.back()
-        }
-    }
+//    override fun onBackPressed() {
+//        val fragment = navHostFragment!!.childFragmentManager.fragments.first()
+//        if (fragment is  BaseFragment<*>) {
+//            fragment.back()
+//        }
+//    }
 
     override fun finish() = super.finish()
 
     override fun navigate(bundle: BundleWrapper<*>, actionId: Int) {
-        if (bundle is BundleWrapper.Empty) {
-            findNavController(R.id.host_fragment).navigate(ru.zinoview.navigation.R.id.moviesFragment)
-        } else {
-            findNavController(R.id.host_fragment).navigate(actionId,bundle.bundle())
-        }
+//        if (bundle is BundleWrapper.Empty) {
+//            findNavController(R.id.host_fragment).navigate(ru.zinoview.navigation.R.id.moviesFragment)
+//        } else {
+//            findNavController(R.id.host_fragment).navigate(actionId,bundle.bundle())
+//        }
     }
 
 }
